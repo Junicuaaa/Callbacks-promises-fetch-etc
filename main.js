bb1dbb47f8eef6a36cb745f188f5ba5d811ba0d2
JuniorSantiagoCampus <juniorcarvajalino1213@gmail.com>
2023-04-18 09:19:58 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+ //CALLBACKS//
+
+const user = "JuniorSantiagoCampus";
+const password = "123456789";
+const wait = () =>{
+    console.log("Espere mientras procesamos su solicitud");
+}
+const result = (p1)=>{
+    return (p1 === true)? "Ingreso Exitoso" : "Ingreso fallido, credenciales erradas"
+} 
+
+
+const request = (p1, p2, callback1, callback2)=>{
+    setTimeout(()=>{
+        let userValidation = (p1 === "JuniorSantiagoCampus")? true : false;
+        let userPassword = (p2 === "123456789")? true: false; 
+        const response = (userPassword === userValidation);
+        console.log(callback1(response));
+    }, 3000);
+    callback2();   
+}
+
+request(user, password, result, wait);
